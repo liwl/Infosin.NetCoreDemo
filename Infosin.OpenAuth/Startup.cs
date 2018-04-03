e5aa2c8b163f24ba96b1f46c6d9f1dcba7b49b93
liwl <lipengfeng0012@163.com>
2018-04-03 23:01:39 +0800

    修改

--- Infosin.OpenAuth/Startup.cs
+++ Infosin.OpenAuth/Startup.cs
@@ -27,7 +27,7 @@ namespace Infosin.OpenAuth
         {
             Configuration = configuration;
 
-            //初始化映射关系1
+            //初始化映射关系12
             MapperInitialize.Initialize();
         }
 
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
  * 采用net core 2.0+mvc+仓储模式 实现
  * 前端采用AdminLTE 使用bower管理js框架
  * 项目解决方案为vs2017
+ * 测试项目
 #
